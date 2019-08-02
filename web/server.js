4f16411c47182856617d5946e53aef0e6d02f706
SenalAriyaratne <ariyaratne.senal@gmail.com>
2019-08-02 21:45:32 +1000

    NEW: Improved server.js

--- web/server.js
+++ web/server.js
@@ -6,10 +6,13 @@ const base = `${__dirname}/public`;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.sendFile(`${base}/device-list.html`);
 });
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
-   });
-   
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(`${base}/404.html`);
+});
